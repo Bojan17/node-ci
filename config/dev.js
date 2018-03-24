6c367fee271ca7148684db44c5ebd2154f59dabb
Bojan17 <bojan.kovacevic.pv@gmail.com>
2018-03-23 18:31:59 -0800

    config edited

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://bojan93:bojan93@ds115219.mlab.com:15219/bloggerdev',
   cookieKey: '123123123',
-  redisUrl : 'redis://127.0.0.1:6379';
+  redisUrl : 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAI73WD3GCMQRHSKLQ',
+  secretKeyId: 'PVb3a2xG8PElfXsz/3BQW53KRNnYfYeSYovNlWmm'
 };
